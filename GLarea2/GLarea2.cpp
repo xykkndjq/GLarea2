d6e1445000a43b24e6382e0e05b54d480542f039
xykkndjq <xykkndjq@gmail.com>
2019-02-22 15:07:12 +0800

    Add project files.

---
+++ GLarea2/GLarea2.cpp
@@ -0,0 +1,6 @@
+#include "stdafx.h"
+#include "GLarea2.h"
+
+GLarea2::GLarea2()
+{
+}
---
+++ GLarea2/GLarea2.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "glarea2_global.h"
+
+class GLAREA2_EXPORT GLarea2
+{
+public:
+	GLarea2();
+};
